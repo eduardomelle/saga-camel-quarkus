cf5d6ea635c40929e33f3a359487020388aa5976
Eduardo <eduardomelle@gmail.com>
2022-11-01 07:22:30 -0300

    Parte #3 - Deixe o Camel e o Quarkus cuidarem das suas sagas

--- compra-saga/src/main/java/br/com/eduardomelle/SagaRoute.java
+++ compra-saga/src/main/java/br/com/eduardomelle/SagaRoute.java
@@ -3,7 +3,9 @@ package br.com.eduardomelle;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.SagaPropagation;
 import org.apache.camel.saga.CamelSagaService;
 import org.apache.camel.saga.InMemorySagaService;
 
@@ -21,7 +23,24 @@ public class SagaRoute extends RouteBuilder {
     CamelSagaService sagaService = new InMemorySagaService();
     getContext().addService(sagaService);
 
-    from("direct:saga").saga().propagation(null);
+    // Saga:
+    from("direct:saga").saga().propagation(SagaPropagation.REQUIRES_NEW).log("Iniciando a transação")
+        .to("direct:newPedido").log("Criando novo pedido")
+        .to("direct:newPedidoValor").log("Reservando o crédito")
+        .to("direct:finaliza").log("Feito!");
+
+    // Pedido Service:
+    from("direct:newPedido").saga().propagation(SagaPropagation.MANDATORY)
+        .compensation("direct:cancelPedido")
+        .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
+        .bean(pedidoService, "newPedido").log("Pedido $<body> criado");
+
+    from("direct:cancelPedido")
+        .transform().header(Exchange.SAGA_LONG_RUNNING_ACTION)
+        .bean(pedidoService, "cancelPedido").log("Pedido $<body> cancelado");
+
+    // Crédito Service:
+
   }
 
 }
